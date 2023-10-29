befadb3099e58809239f422417d3599089c89e23
Loc179 <111257006+Loc179@users.noreply.github.com>
2023-10-29 14:25:39 +0700

    add requirement, benefit

---
+++ Controllers/BenefitController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QL_Ung_Vien.Areas.Identity.Data;
+using QL_Ung_Vien.Models;
+
+namespace QL_Ung_Vien.Controllers
+{
+    public class BenefitController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public BenefitController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: Benefit
+        public async Task<IActionResult> Index()
+        {
+              return db.Benefits != null ? 
+                          View(await db.Benefits.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Benefits'  is null.");
+        }
+
+        // GET: Benefit/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || db.Benefits == null)
+            {
+                return NotFound();
+            }
+
+            var benefit = await db.Benefits
+                .FirstOrDefaultAsync(m => m.benefitID == id);
+            if (benefit == null)
+            {
+                return NotFound();
+            }
+
+            return View(benefit);
+        }
+
+        // GET: Benefit/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Benefit/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("benefitID,benefitContent")] Benefit benefit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(benefit);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(benefit);
+        }
+
+        // GET: Benefit/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || db.Benefits == null)
+            {
+                return NotFound();
+            }
+
+            var benefit = await db.Benefits.FindAsync(id);
+            if (benefit == null)
+            {
+                return NotFound();
+            }
+            return View(benefit);
+        }
+
+        // POST: Benefit/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("benefitID,benefitContent")] Benefit benefit)
+        {
+            if (id != benefit.benefitID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(benefit);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BenefitExists(benefit.benefitID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(benefit);
+        }
+
+        // GET: Benefit/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || db.Benefits == null)
+            {
+                return NotFound();
+            }
+
+            var benefit = await db.Benefits
+                .FirstOrDefaultAsync(m => m.benefitID == id);
+            if (benefit == null)
+            {
+                return NotFound();
+            }
+
+            return View(benefit);
+        }
+
+        // POST: Benefit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (db.Benefits == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Benefits'  is null.");
+            }
+            var benefit = await db.Benefits.FindAsync(id);
+            if (benefit != null)
+            {
+                db.Benefits.Remove(benefit);
+            }
+            
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BenefitExists(int id)
+        {
+          return (db.Benefits?.Any(e => e.benefitID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/RequirementController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QL_Ung_Vien.Areas.Identity.Data;
+using QL_Ung_Vien.Models;
+
+namespace QL_Ung_Vien.Controllers
+{
+    public class RequirementController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public RequirementController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: Requirement
+        public async Task<IActionResult> Index()
+        {
+              return db.Requirements != null ? 
+                          View(await db.Requirements.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Requirements'  is null.");
+        }
+
+        // GET: Requirement/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || db.Requirements == null)
+            {
+                return NotFound();
+            }
+
+            var requirement = await db.Requirements
+                .FirstOrDefaultAsync(m => m.requirementID == id);
+            if (requirement == null)
+            {
+                return NotFound();
+            }
+
+            return View(requirement);
+        }
+
+        // GET: Requirement/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Requirement/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("requirementID,requirementContent")] Requirement requirement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(requirement);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requirement);
+        }
+
+        // GET: Requirement/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || db.Requirements == null)
+            {
+                return NotFound();
+            }
+
+            var requirement = await db.Requirements.FindAsync(id);
+            if (requirement == null)
+            {
+                return NotFound();
+            }
+            return View(requirement);
+        }
+
+        // POST: Requirement/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("requirementID,requirementContent")] Requirement requirement)
+        {
+            if (id != requirement.requirementID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(requirement);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequirementExists(requirement.requirementID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(requirement);
+        }
+
+        // GET: Requirement/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || db.Requirements == null)
+            {
+                return NotFound();
+            }
+
+            var requirement = await db.Requirements
+                .FirstOrDefaultAsync(m => m.requirementID == id);
+            if (requirement == null)
+            {
+                return NotFound();
+            }
+
+            return View(requirement);
+        }
+
+        // POST: Requirement/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (db.Requirements == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Requirements'  is null.");
+            }
+            var requirement = await db.Requirements.FindAsync(id);
+            if (requirement != null)
+            {
+                db.Requirements.Remove(requirement);
+            }
+            
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequirementExists(int id)
+        {
+          return (db.Requirements?.Any(e => e.requirementID == id)).GetValueOrDefault();
+        }
+    }
+}
